5fcf329fd98fedd55add72df183247708674785d
Tamarawillis44 <techietl444@gmail.com>
2022-05-31 08:00:18 -0400

    Initial Commit

---
+++ Guess De Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess De Flag
+//
+//  Created by Tamara laraque on 5/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guess De Flag/Guess_De_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_De_FlagApp.swift
+//  Guess De Flag
+//
+//  Created by Tamara laraque on 5/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Guess_De_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
